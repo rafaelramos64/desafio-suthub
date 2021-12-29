6e3d09e3d32bdc759fbe5e6b75801be338939b5e
rafaelramos64 <rafaelmaggot64@gmail.com>
2021-12-29 19:46:35 -0300

    Fiz adjusts on layout of lang page

--- pages/lang/_lang.vue
+++ pages/lang/_lang.vue
@@ -6,15 +6,23 @@
       </v-col>
     </v-row>
 
-    <v-row class="text-center mb-2">
-      <v-col cols="12" class="d-flex justify-center align-center">
-        <h3 class="ml-2 mt-1 h3">
-          Países que falam <span class="capitalize-native-name">{{ getSelectedLanguage.nativeName }}</span>
-        </h3>
-      </v-col>
+    <v-row class="text-center mb-2" justify="center">
+      <v-col cols="12" md="9" xl="8" class="d-flex justify-center align-center">
+        <v-card max-width="1200px" class="card-lang py-5">
+          <v-row justify="center">
+            <v-col cols="12">
+              <h3 class="ml-2 mt-1 h3">
+                Países que falam <span class="capitalize-native-name">{{ getSelectedLanguage.nativeName }}</span>
+              </h3>
+            </v-col>
 
-      <v-col cols="6" sm="4" v-for="country in getCountries" :key="country.name">
-        {{ country.name }}
+            <v-divider></v-divider>
+
+            <v-col cols="6" sm="4" v-for="country in getCountries" :key="country.name">
+              {{ country.name }}
+            </v-col>
+          </v-row>
+        </v-card>
       </v-col>
     </v-row>
   </div>
@@ -48,5 +56,9 @@ export default {
 
 .capitalize-native-name {
   text-transform: capitalize;
+}
+
+.card-lang {
+  border-radius: 0.9rem;
 }
 </style>
